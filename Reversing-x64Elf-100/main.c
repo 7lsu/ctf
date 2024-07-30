0f958b30feffc99a3a61a4530a9cab314f48b055
7lsu <a@7lsu.com>
2024-07-30 17:52:05 +0800

    add: GFSJ0240

---
+++ Reversing-x64Elf-100/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+int main() {
+    char s[264] = "Code_Talkers";
+    char *v3[4];
+    v3[0] = "Dufhbmf";
+    v3[1] = "pG`imos";
+    v3[2] = "ewUglpt";
+    int i;
+    for ( i = 0; i <= 11; ++i )
+    {
+        // if ( *(v3[i % 3] + 2 * (i / 3)) - *(i + s) != 1 )
+        //     return 1LL;
+        // int x = *(v3[i % 3] + 2 * (i / 3));
+        // printf("x = %d\n", x);
+        // printf("y = x - 1 = %d\n", x - 1);
+    }
+    return 0;
+}
